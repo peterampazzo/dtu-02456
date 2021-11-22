ec6c477c772e303aae705f43c41d2fb3ddfb5504
Pietro Rampazzo <pietro@rampazzo.co>
2021-11-22 11:48:34 +0100

    batch spliting

--- src/create_folders.py
+++ src/create_folders.py
@@ -2,45 +2,46 @@
 import csv
 import shutil
 import os
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="[%(asctime)s] - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    stream=sys.stdout,
+)
 
 training, test, val = [], [], []
 
-filename = 'data_split.csv'
+root = "/work1/fbohy/Helmet"
+filename = f"{root}/data_split.csv"
 
-with open(filename, 'r') as csvfile:
+folders = [
+    f"{root}/Myanmar_data/training_set/image/",
+    f"{root}/Myanmar_data/test_set/image/",
+    f"{root}/Myanmar_data/val_set/image/",
+]
+
+for i in folders:
+    if not os.path.exists(i):
+        os.makedirs(i)
+
+with open(filename, "r") as csvfile:
     datareader = csv.reader(csvfile)
     next(datareader)
     for annot, split_type in datareader:
-        
-        # get the annotation file
-#         print(annot)
-        # get the type of folder it should be in
-#         print(split_type)
-    
-        # fetch the annotation from part_1 folder and put it in the right folder 
-#         for foldername in os.listdir('part_1'):
-        folder_source = f'part_1/{annot}/'
-#             if foldername == annot:
-                #put the annotation in the right folder
-        if (split_type == 'training'):
+        folder_source = f"{root}/images/{annot}/"
+        logging.debug(f"Folder: {folder_source} - {split_type}")
+
+        if split_type == "training":
             training.append(annot)
-            folder_destination = 'Myanmar_data/training_set/image/'
-            shutil.move(folder_source, folder_destination)
+            shutil.move(folder_source, folders[0])
 
-        elif (split_type == 'test'):
+        elif split_type == "test":
             test.append(annot)
-            folder_destination = 'Myanmar_data/test_set/image/'
-            shutil.move(folder_source, folder_destination)
+            shutil.move(folder_source, folders[1])
 
-
-        elif (split_type == 'validation'):
+        elif split_type == "validation":
             val.append(annot)
-            folder_destination = 'Myanmar_data/val_set/image/'
-            shutil.move(folder_source, folder_destination)
-#             else:
-#                 continue
-
-
-print(training)
-print(test)
-print(val)
+            shutil.move(folder_source, folders[2])
