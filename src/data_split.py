efba35c00d59533bcae70ca6790aed55fbbd72c1
Pietro Rampazzo <pietro@rampazzo.co>
2021-11-24 14:48:57 +0100

    improve directories structure

--- src/data_split.py
+++ src/data_split.py
@@ -14,19 +14,25 @@ logging.basicConfig(
 )
 
 
-def load_from_csv(path: str, video_id_col: int = 0, set_col: int = 1):
+def load_from_csv(
+    path: str, filename: str, directory: str, video_id_col: int = 0, set_col: int = 1
+):
+    file_path = os.path.join(path, filename)
+
     logging.info("Splitting data from csv file.")
-    logging.debug(f"File loadede: {path}.")
+    logging.debug(f"File loaded: {file_path}.")
+
     train, val, test = [], [], []
-    with open(path, "r") as read_obj:
+    with open(file_path, "r") as read_obj:
         csv_reader = reader(read_obj)
         for row in csv_reader:
+            folder_path = os.path.join(path, directory, row[video_id_col])
             if row[set_col] == "training":
-                train.append(row[video_id_col])
+                train.append(folder_path)
             if row[set_col] == "validation":
-                val.append(row[video_id_col])
+                val.append(folder_path)
             if row[set_col] == "test":
-                test.append(row[video_id_col])
+                test.append(folder_path)
     return train, val, test
 
 
@@ -64,15 +70,17 @@ def split_data(destination: str, train: list, val: list, test: list):
 
     data = {
         "train": {
-            "dest": f"{destination}/Myanmar_data/training_set/image/",
+            "dest": f"{destination}/training_set/image/",
             "folders": train,
         },
         "test": {
-            "dest": f"{destination}/Myanmar_data/test_set/image/",
+            "dest": f"{destination}/test_set/image/",
             "folders": test,
         },
-        "val": {"dest": f"{destination}/Myanmar_data/val_set/image/", "folders": val},
+        "val": {"dest": f"{destination}/val_set/image/", "folders": val},
     }
+
+    create_directory(destination)
 
     for i in data:
         create_directory(data[i]["dest"])
@@ -82,8 +90,11 @@ def split_data(destination: str, train: list, val: list, test: list):
 if __name__ == "__main__":
     logging.info("Application started.")
 
-    origin = "/work1/fbohy/Helmet/"
-    destination = "/work3/s203257/"
-    # load_from_csv()
-    train, val, test = generate_random_sets(origin, 0.7, 0.1)
+    project = "Nepal"
+    main_folder = "/work3/s203257/"
+    origin = f"{main_folder}/{project}_raw/"
+    destination = f"{main_folder}/{project}_processed/"
+
+    train, val, test = load_from_csv(origin, "annotation.csv", f"{project}_raw")
+    # train, val, test = generate_random_sets(origin, 0.7, 0.1)
     split_data(destination, train, val, test)
