0163dd7700c7c2443c9b18de9764b6b0386853ea
Pietro Rampazzo <pietro@rampazzo.co>
2021-11-29 14:30:43 +0100

    bugs fix

--- src/classification_encode.py
+++ src/classification_encode.py
@@ -14,16 +14,23 @@ from torch.optim import lr_scheduler
 from tqdm import tqdm
 from utils import *
 
-project = "Nepal"
+project = "Myanmar"
 main_folder = "/work3/s203257"
 origin = f"{main_folder}/{project}_raw/"
 destination = f"{main_folder}/{project}_processed/"
 
 batch_size = 32
 input_shape = (192, 192, 3)
-train_ids = pd.read_csv(f"{main_folder}/{project}_annot/training_set.csv")
-val_ids = pd.read_csv(f"{main_folder}/{project}_annot/val_set.csv")
-test_ids = pd.read_csv(f"{main_folder}/{project}_annot/test_set.csv")
+train_ids = pd.read_csv(
+    f"{main_folder}/{project}_annot/training_set.csv",
+)  # dtype={'frame_id': 'str'})
+val_ids = pd.read_csv(
+    f"{main_folder}/{project}_annot/val_set.csv",
+)  # dtype={'frame_id': 'str'})
+test_ids = pd.read_csv(
+    f"{main_folder}/{project}_annot/test_set.csv",
+)  # dtype={'frame_id': 'str'})
+
 print("training data:", len(train_ids))
 print("valid data:", len(val_ids))
 print("test data:", len(test_ids))
@@ -193,7 +200,7 @@ def train_model(model, optimizer, scheduler, num_epochs=1):
     return model, epoch_ACCs
 
 
-savepath = "save_model/nepal-encode.pt"
+savepath = f"save_model/{project}-encode.pt"
 model_ft, epoch_ACCs = train_model(
     model_ft, optimizer_ft, exp_lr_scheduler, num_epochs=10
 )
@@ -201,7 +208,7 @@ torch.save(model_ft.state_dict(), savepath)
 # torch.save(model_ft, savepath)
 print(epoch_ACCs)
 
-savepath = "save_model/nepal-encode.pt"
+savepath = f"save_model/{project}-encode.pt"
 model_ft.load_state_dict(torch.load(savepath))
 model_ft.eval()
 
